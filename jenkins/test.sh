65f745894fc06932ed4299e7405d0794ade6e619
romanca <r.jamrisko@gmail.com>
2023-08-24 17:46:23 +0200

    test sh

---
+++ jenkins/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script performs the testing of your Node.js and React application.
+# You can add any testing commands or procedures you need here.
+
+# Example: Run tests for the frontend
+cd frontend
+npm install
+npm test
+
+# Example: Run tests for the backend
+cd backend
+npm install
+npm test
